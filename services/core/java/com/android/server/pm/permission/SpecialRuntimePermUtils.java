c01fd6f2992e43519d5098519fd9b35b6b17a38a
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-02-09 18:53:11 +0200

    infrastructure for special runtime permissions
    Squashed with 8320ef57c9d47c6933c753b69c0279a960752014 by
    Andrew Gunnerson <accounts+github@chiller3.com>

---
+++ services/core/java/com/android/server/pm/permission/SpecialRuntimePermUtils.java
@@ -0,0 +1,62 @@
+package com.android.server.pm.permission;
+
+import android.Manifest;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.PackageManagerInternal;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.ArraySet;
+import android.util.EmptyArray;
+import android.util.LruCache;
+import android.util.Slog;
+import android.util.SparseArray;
+
+import com.android.internal.pm.pkg.component.ParsedUsesPermission;
+import com.android.server.LocalServices;
+import com.android.server.pm.PackageManagerService;
+import com.android.server.pm.UserManagerInternal;
+import com.android.server.pm.pkg.AndroidPackage;
+import com.android.server.pm.pkg.PackageState;
+import com.android.server.pm.pkg.PackageStateInternal;
+
+import java.util.List;
+
+public class SpecialRuntimePermUtils {
+    private static final String TAG = SpecialRuntimePermUtils.class.getSimpleName();
+
+    private static final ArraySet<String> specialRuntimePermissions = new ArraySet<>(new String[] {
+    });
+
+    public static boolean isSpecialRuntimePermission(String permission) {
+        return specialRuntimePermissions.contains(permission);
+    }
+
+    public static String[] getAll() {
+        return specialRuntimePermissions.toArray(EmptyArray.STRING);
+    }
+
+    public static boolean shouldAutoGrant(Context ctx, String packageName, int userId, String perm) {
+        if (!isSpecialRuntimePermission(perm)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static int getFlags(PackageManagerService pm, AndroidPackage pkg, PackageState pkgState, int userId) {
+        int flags = 0;
+
+        for (ParsedUsesPermission perm : pkg.getUsesPermissionMapping().values()) {
+            String name = perm.getName();
+            switch (name) {
+                default:
+                    continue;
+            }
+        }
+
+        return flags;
+    }
+
+    private SpecialRuntimePermUtils() {}
+}
