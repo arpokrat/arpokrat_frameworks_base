2ceab2efdf0aa71a83f5a8b1398b6d6122e7f7d5
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-16 15:38:31 +0300

    support redirecting ContentProvider calls to another provider

--- core/java/android/app/ContextImpl.java
+++ core/java/android/app/ContextImpl.java
@@ -21,6 +21,7 @@ import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 import static android.os.StrictMode.vmIncorrectContextUseEnabled;
 import static android.permission.flags.Flags.shouldRegisterAttributionSource;
 import static android.view.WindowManager.LayoutParams.WindowType;
+import static com.android.internal.app.ContentProviderRedirector.translateContentProviderAuthority;
 import static com.android.internal.gmscompat.GmcDebug.maybeLogSendBroadcast;
 import static com.android.internal.gmscompat.GmcDebug.maybeLogStartService;
 
@@ -3980,6 +3981,7 @@ class ContextImpl extends Context {
         @Override
         @UnsupportedAppUsage
         protected IContentProvider acquireProvider(Context context, String auth) {
+            auth = translateContentProviderAuthority(auth);
             return mMainThread.acquireProvider(context,
                     ContentProvider.getAuthorityWithoutUserId(auth),
                     resolveUserIdFromAuthority(auth), true);
@@ -3987,6 +3989,7 @@ class ContextImpl extends Context {
 
         @Override
         protected IContentProvider acquireExistingProvider(Context context, String auth) {
+            auth = translateContentProviderAuthority(auth);
             return mMainThread.acquireExistingProvider(context,
                     ContentProvider.getAuthorityWithoutUserId(auth),
                     resolveUserIdFromAuthority(auth), true);
@@ -3999,6 +4002,7 @@ class ContextImpl extends Context {
 
         @Override
         protected IContentProvider acquireUnstableProvider(Context c, String auth) {
+            auth = translateContentProviderAuthority(auth);
             return mMainThread.acquireProvider(c,
                     ContentProvider.getAuthorityWithoutUserId(auth),
                     resolveUserIdFromAuthority(auth), false);
