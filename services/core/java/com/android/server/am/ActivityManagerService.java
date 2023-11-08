3cae3ac8337744f928b2f0648a57f9d86ad7dbfd
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-11-08 21:01:31 +0200

    notify the user about notable system journal entries
    Show notifications about the following events:
    - kernel crash (from last_kmsg) (optional)
    - file system check error
    - system_server crash (Java or native)
    - system app native crash (optional, except for memory tagging crashes)
    - non-app process native crash (optional, except for memory tagging crashes)
    - some of hardened_malloc app crashes (those that are caused by fatal_error() call in hardened_malloc
    after it detects an app error)
    
    These events are recorded in the system journal ("DropBox") already, this commit surfaces them to
    the user.

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -2208,6 +2208,7 @@ public class ActivityManagerService extends IActivityManager.Stub
                     mService.mCrashRecoveryHelper.registerConnectivityModuleHealthListener();
                 }
                 mService.scheduleHomeTimeout();
+                com.android.server.ext.SystemJournalNotif.onSystemServerInited();
             }
         }
 
