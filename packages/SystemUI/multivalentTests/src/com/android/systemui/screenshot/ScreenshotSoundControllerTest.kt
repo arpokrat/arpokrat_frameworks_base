2c28b399ee9df9ecc2faab8e9b950d707c3d8786
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-03-08 13:26:52 +0200

    SystemUI: make screenshot sound use sound effects setting

--- packages/SystemUI/multivalentTests/src/com/android/systemui/screenshot/ScreenshotSoundControllerTest.kt
+++ packages/SystemUI/multivalentTests/src/com/android/systemui/screenshot/ScreenshotSoundControllerTest.kt
@@ -20,6 +20,7 @@ import android.media.MediaPlayer
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.android.systemui.SysuiTestCase
+import com.android.systemui.shared.settings.data.repository.FakeSystemSettingsRepository
 import com.android.systemui.util.mockito.mock
 import com.android.systemui.util.mockito.whenever
 import java.lang.IllegalStateException
@@ -42,6 +43,7 @@ class ScreenshotSoundControllerTest : SysuiTestCase() {
     private val mediaPlayer = mock<MediaPlayer>()
     private val bgDispatcher = UnconfinedTestDispatcher()
     private val scope = TestScope(bgDispatcher)
+    private val fakeSystemSettingsRepo = FakeSystemSettingsRepository()
 
     @Before
     fun setup() {
@@ -106,5 +108,5 @@ class ScreenshotSoundControllerTest : SysuiTestCase() {
         }
 
     private fun createController() =
-        ScreenshotSoundControllerImpl(soundProvider, scope, bgDispatcher)
+        ScreenshotSoundControllerImpl(soundProvider, fakeSystemSettingsRepo, scope, bgDispatcher)
 }
