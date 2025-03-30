e4b97855a495ca163a0f8da60618dd604e581ee9
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-03-30 18:08:45 +0300

    Revert "SettingsProviderHooks: Infrastructure for hooking settings state init"
    This reverts commit d56df134945bced717621c276d85ff96a0a0a970.

--- packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
+++ packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
@@ -3344,7 +3344,6 @@ public class SettingsProvider extends ContentProvider {
             // Upgrade the settings to the latest version.
             UpgradeController upgrader = new UpgradeController(userId);
             upgrader.upgradeIfNeededLocked();
-            SettingsProviderHooks.onSettingsStateInit(SettingsRegistry.this, userId);
             return true;
         }
 
