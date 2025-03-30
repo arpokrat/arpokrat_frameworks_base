1a62a3adb2e27f54b36ff82ed8f28b15c6bea1eb
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-03-30 18:16:59 +0300

    don't allow enabling the "add users when locked" setting

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13265,6 +13265,7 @@ public final class Settings {
          * @hide
          */
         @Readable
+        @Protected(immutableValue = "0")
         public static final String ADD_USERS_WHEN_LOCKED = "add_users_when_locked";
 
         /**
