5cfbf2393a34ba0fce9f873c3a774e8cd974e7c4
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-03-31 11:31:12 +0300

    fixup! add BinderWrapper
    Add missing @Override annotations, which don't change functionality but ensure that compilation
    fails if base methods gets removed or renamed.

--- core/java/android/os/BinderWrapper.java
+++ core/java/android/os/BinderWrapper.java
@@ -14,44 +14,54 @@ public class BinderWrapper implements IBinder {
         this.base = base;
     }
 
+    @Override
     public boolean transact(int code, @NonNull Parcel data, @Nullable Parcel reply, int flags) throws RemoteException {
         return base.transact(code, data, reply, flags);
     }
 
     @Nullable
+    @Override
     public IInterface queryLocalInterface(@NonNull String descriptor) {
         return base.queryLocalInterface(descriptor);
     }
 
     @Nullable
+    @Override
     public String getInterfaceDescriptor() throws RemoteException {
         return base.getInterfaceDescriptor();
     }
 
+    @Override
     public boolean pingBinder() {
         return base.pingBinder();
     }
 
+    @Override
     public boolean isBinderAlive() {
         return base.isBinderAlive();
     }
 
+    @Override
     public void dump(@NonNull FileDescriptor fd, @Nullable String[] args) throws RemoteException {
         base.dump(fd, args);
     }
 
+    @Override
     public void dumpAsync(@NonNull FileDescriptor fd, @Nullable String[] args) throws RemoteException {
         base.dumpAsync(fd, args);
     }
 
+    @Override
     public void shellCommand(@Nullable FileDescriptor in, @Nullable FileDescriptor out, @Nullable FileDescriptor err, @NonNull String[] args, @Nullable ShellCallback shellCallback, @NonNull ResultReceiver resultReceiver) throws RemoteException {
         base.shellCommand(in, out, err, args, shellCallback, resultReceiver);
     }
 
+    @Override
     public void linkToDeath(@NonNull DeathRecipient recipient, int flags) throws RemoteException {
         base.linkToDeath(recipient, flags);
     }
 
+    @Override
     public boolean unlinkToDeath(@NonNull DeathRecipient recipient, int flags) {
         return base.unlinkToDeath(recipient, flags);
     }
