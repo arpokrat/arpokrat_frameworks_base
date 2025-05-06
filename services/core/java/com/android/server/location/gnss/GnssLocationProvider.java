GrapheneOS-Archive/platform_frameworks_base-old

add GNSS PSDS setting

services/core/java/com/android/server/location/gnss/GnssLocationProvider.java
@@ -61,6 +61,7 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.database.ContentObserver;
 import android.ext.settings.ExtSettings;
+import android.ext.settings.GnssConstants;
 import android.ext.settings.IntSetting;
 import android.location.GnssCapabilities;
 import android.location.GnssStatus;
@@ -492,6 +493,18 @@ public class GnssLocationProvider extends AbstractLocationProvider implements
         };
 
         ExtSettings.GNSS_SUPL.registerObserver(mContext, suplSettingObserver, mHandler);
+
+        if (ExtSettings.isStandardGnssPsds(mContext)) {
+            Consumer<IntSetting> psdsSettingObserver = setting -> {
+                Slog.d(TAG, "PSDS setting changed, value: " + setting.get(mContext));
+                if (!isPsdsEnabled()) {
+                    Slog.d(TAG, "PSDS is disabled");
+                }
+                reloadGpsProperties();
+            };
+
+            ExtSettings.GNSS_PSDS_STANDARD.registerObserver(mContext, psdsSettingObserver, mHandler);
+        }
     }
 
     /** Called when system is ready. */
@@ -585,7 +598,7 @@ public class GnssLocationProvider extends AbstractLocationProvider implements
     private void onNetworkAvailable() {
         mNtpTimeHelper.onNetworkAvailable();
         // Download only if supported, (prevents an unnecessary on-boot download)
-        if (mSupportsPsds) {
+        if (mSupportsPsds && isPsdsEnabled()) {
             synchronized (mLock) {
                 for (int psdsType : mPendingDownloadPsdsTypes) {
                     postWithWakeLockHeld(() -> handleDownloadPsdsData(psdsType));
@@ -678,6 +691,10 @@ public class GnssLocationProvider extends AbstractLocationProvider implements
         if (!mSupportsPsds) {
             // native code reports psds not supported, don't try
             Log.d(TAG, "handleDownloadPsdsData() called when PSDS not supported");
+            return;
+        }
+        if (!isPsdsEnabled()) {
+            Log.d(TAG, "handleDownloadPsdsData() called when PSDS is disabled");
             return;
         }
         if (!mNetworkConnectivityHandler.isDataNetworkConnected()) {
@@ -1762,5 +1779,10 @@ public class GnssLocationProvider extends AbstractLocationProvider implements
         mGnssVisibilityControl.reportNfwNotification(proxyAppPackageName, protocolStack,
                 otherProtocolStackName, requestor, requestorId, responseType, inEmergencyMode,
                 isCachedLocation);
+    }
+
+    private boolean isPsdsEnabled() {
+        return ExtSettings.isStandardGnssPsds(mContext) &&
+                ExtSettings.GNSS_PSDS_STANDARD.get(mContext) != GnssConstants.PSDS_DISABLED;
     }
 }

arpokrat/arpokrat_frameworks_base

services/core/java/com/android/server/location/gnss/GnssLocationProvider.java
@@ -505,6 +505,18 @@ public class GnssLocationProvider extends AbstractLocationProvider implements
         };
 
         GnssSettings.SUPL_SETTING.registerObserver(mContext, mHandler, suplSettingObserver);
+
+        if (GnssSettings.isStandardPsds(mContext)) {
+            Consumer<IntSetting> psdsSettingObserver = setting -> {
+                Slog.d(TAG, "PSDS setting changed, value: " + setting.get(mContext));
+                if (!isPsdsEnabled()) {
+                    Slog.d(TAG, "PSDS is disabled");
+                }
+                reloadGpsProperties();
+            };
+
+            GnssSettings.STANDARD_PSDS_SETTING.registerObserver(mContext, mHandler, psdsSettingObserver);
+        }
     }
 
     /** Called when system is ready. */
@@ -679,7 +691,7 @@ public class GnssLocationProvider extends AbstractLocationProvider implements
     private void onNetworkAvailable() {
         mNetworkTimeHelper.onNetworkAvailable();
         // Download only if supported, (prevents an unnecessary on-boot download)
-        if (mSupportsPsds) {
+        if (mSupportsPsds && isPsdsEnabled()) {
             synchronized (mLock) {
                 for (int psdsType : mPendingDownloadPsdsTypes) {
                     postWithWakeLockHeld(() -> handleDownloadPsdsData(psdsType));
@@ -772,6 +784,10 @@ public class GnssLocationProvider extends AbstractLocationProvider implements
         if (!mSupportsPsds) {
             // native code reports psds not supported, don't try
             Log.d(TAG, "handleDownloadPsdsData() called when PSDS not supported");
+            return;
+        }
+        if (!isPsdsEnabled()) {
+            Log.d(TAG, "handleDownloadPsdsData() called when PSDS is disabled");
             return;
         }
         if (!mNetworkConnectivityHandler.isDataNetworkConnected()) {
@@ -1849,5 +1865,10 @@ public class GnssLocationProvider extends AbstractLocationProvider implements
         mGnssVisibilityControl.reportNfwNotification(proxyAppPackageName, protocolStack,
                 otherProtocolStackName, requestor, requestorId, responseType, inEmergencyMode,
                 isCachedLocation);
+    }
+
+    private boolean isPsdsEnabled() {
+        return GnssSettings.isStandardPsds(mContext) &&
+                GnssSettings.STANDARD_PSDS_SETTING.get(mContext) != GnssSettings.PSDS_DISABLED;
     }
 }