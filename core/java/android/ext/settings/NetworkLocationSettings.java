e918eed9e2bf3a7aadac51abb21e285bc6f0836b
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2026-01-22 20:44:18 -0800

    update network location setting names

--- core/java/android/ext/settings/NetworkLocationSettings.java
+++ core/java/android/ext/settings/NetworkLocationSettings.java
@@ -6,12 +6,12 @@ import android.provider.Settings;
 public class NetworkLocationSettings {
 
     public static final int NETWORK_LOCATION_DISABLED = 0;
-    public static final int NETWORK_LOCATION_SERVER_APPLE = 1;
-    public static final int NETWORK_LOCATION_SERVER_GRAPHENEOS_PROXY = 2;
+    public static final int NETWORK_LOCATION_APPLE = 1;
+    public static final int NETWORK_LOCATION_GRAPHENEOS_APPLE_PROXY = 2;
 
     public static final IntSetting NETWORK_LOCATION_SETTING = new IntSetting(
             Setting.Scope.GLOBAL, Settings.Global.NETWORK_LOCATION,
             NETWORK_LOCATION_DISABLED, // default
-            NETWORK_LOCATION_SERVER_GRAPHENEOS_PROXY, NETWORK_LOCATION_SERVER_APPLE, NETWORK_LOCATION_DISABLED // valid values
+            NETWORK_LOCATION_GRAPHENEOS_APPLE_PROXY, NETWORK_LOCATION_APPLE, NETWORK_LOCATION_DISABLED // valid values
     );
 }
