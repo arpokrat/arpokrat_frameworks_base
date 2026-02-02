fa0265d77eb94fc261a4d9849e25662d4a5ce84b
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2026-02-02 01:23:14 -0800

    add Setup Wizard to KnownSystemPackages

--- core/java/android/ext/KnownSystemPackage.java
+++ core/java/android/ext/KnownSystemPackage.java
@@ -12,12 +12,14 @@ public interface KnownSystemPackage {
     int SETTINGS = 0;
     int SHELL = 1;
     int SYSTEM_UI = 2;
+    int SETUP_WIZARD = 3;
 
     /** @hide */
     @IntDef(value = {
             SETTINGS,
             SHELL,
             SYSTEM_UI,
+            SETUP_WIZARD,
     })
     @Retention(RetentionPolicy.SOURCE)
     @interface Enum {}
