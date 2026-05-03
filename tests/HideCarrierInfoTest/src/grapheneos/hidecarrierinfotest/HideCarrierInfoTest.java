cf696fcd1d4b1b4681a3359f36babc79bf978f7a
maade93791 <70593890+maade69@users.noreply.github.com>
2026-05-04 02:02:38 +0300

    HideCarrierInfoTest: include bionic sysprop override tests

--- tests/HideCarrierInfoTest/src/grapheneos/hidecarrierinfotest/HideCarrierInfoTest.java
+++ tests/HideCarrierInfoTest/src/grapheneos/hidecarrierinfotest/HideCarrierInfoTest.java
@@ -9,6 +9,7 @@ import com.android.tradefed.testtype.junit4.DeviceTestRunOptions;
 
 import grapheneos.hardeningtest.TestUtils;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,6 +18,12 @@ public class HideCarrierInfoTest extends BaseHostJUnit4Test {
 
     private static final String TEST_PACKAGE = "app.grapheneos.hidecarrierinfotest";
     private static final String DEVICE_TEST_CLASS = TEST_PACKAGE + ".HideCarrierInfoDeviceTest";
+
+    private static final String BIONIC_TEST_BINARY =
+            "/data/nativetest64/bionic-sysprop-tests/bionic-sysprop-tests";
+    private static final String BIONIC_HCI_FILTER =
+            "properties.__system_property_add_extended_override:" +
+            "properties.__system_property_update_extended_override_denylist";
 
     private void runDeviceTest(String methodName) {
         var opts = new DeviceTestRunOptions(TEST_PACKAGE);
@@ -34,6 +41,22 @@ public class HideCarrierInfoTest extends BaseHostJUnit4Test {
                 GosPackageStateFlag.HIDE_CARRIER_INFO,
                 GosPackageStateFlag.HIDE_CARRIER_INFO_NON_DEFAULT,
                 isSet);
+    }
+
+    // covers new extended sysprop overrides area
+    // + inheritance of ro.appcompat
+    // see bionic/tests/system_properties_test.cpp
+    // __system_property_add_extended_override()
+    // __system_property_update_extended_override_denylist()
+    @Test
+    public void testBionicProperties() throws DeviceNotAvailableException {
+        if (!getDevice().doesFileExist(BIONIC_TEST_BINARY)) {
+            return;
+        }
+        var result = getDevice().executeShellV2Command(
+                BIONIC_TEST_BINARY + " --gtest_filter=" + BIONIC_HCI_FILTER);
+        Assert.assertEquals(result.getStdout() + result.getStderr(),
+                0L, (long) result.getExitCode());
     }
 
     @Test
